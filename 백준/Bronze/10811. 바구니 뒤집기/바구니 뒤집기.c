fa6621c209492f7caa6c07e06efae8f9f7092758
Zanghyeoun Shim <zhanghyeoun1@gmail.com>
2025-05-07 17:04:12 -0600

    Update 바구니 뒤집기.c

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.c
@@ -1,30 +1,38 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int basket[101] = {0,};
-    int N,M,i,j;
+int main() {
+    int num_arr, num, i, j;
+    int arr[101] = {0, };
     int temp;
-
-    scanf("%d %d", &N, &M);
-
-    for (int a = 0; a < N; a++){
-        basket[a] = a+1;
+    // b : basket's number 
+    scanf("%d %d", &num_arr, &num);
+    
+    
+    // create array
+    for (int a=0; a<num_arr; a++) {
+        arr[a] = a+1;
     }
 
-    for (int b = 0; b < M; b++){
+    // input from user to decide make reverse from where and to
+    for (int b=0; b<num; b++) {
         scanf("%d %d", &i, &j);
-        for (int c = i-1; c < j; c++){
-            temp = basket[c];
-            basket[c] = basket[j-1];
-            basket[j-1] = temp;
-            j--;
+            for (int c=i-1; c<j; c++){
+                temp = arr[c];
+                arr[c] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
         }
     }
-
-    for (int d = 0; d < N; d++){
-        printf("%d ", basket[d]);
+    
+    /*** logic explanation ********* 
+    // i,j : 0,5->1,4->2,3 switching 
+    ********************************/
+    
+    // output completed array 
+    for (int i=0; i<num_arr; i++){
+        printf("%d ", arr[i]);
     }
-
+  
     return 0;
 }
